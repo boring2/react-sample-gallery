c64a27717c29361e3b878e4ddbcedd56f14eb726
boring <code_zhen@163.com>
2016-09-11 22:54:10 +0800

    Complete controller unit

--- src/components/Main.js
+++ src/components/Main.js
@@ -91,13 +91,29 @@ class ControllerUnit extends React.Component {
   }
 
   handleClick(e) {
+    //如果点击的是当前正在选中态的按钮,则翻转图片, 否则居中图片.
+    if (this.props.arrange.isCenter) {
+      this.props.inverse();
+    } else {
+      this.props.center();
+    }
     e.preventDefault();
     e.stopPropagation();
   }
 
   render() {
+    let controllerUnitClassName = "controller-unit";
+
+    //如果对应的是居中图片, 显示控制按钮的翻转态
+    if (this.props.arrange.isCenter) {
+      controllerUnitClassName += ' is-center';
+      //如果同时对应的是翻转图片,显示控制按钮的翻转态
+      if (this.props.arrange.isInverse) {
+        controllerUnitClassName += ' is-inverse';
+      }
+    }
     return (
-      <span className="controller-unit" onClick={this.handleClick.bind(this)}></span>
+      <span className= {controllerUnitClassName} onClick={this.handleClick.bind(this)}></span>
     );
   }
 }
@@ -293,8 +309,8 @@ class GalleryByReactApp extends React.Component {
         }
       }
       imgFigures.push(<ImgFigure key={index} data={value} ref={'imgFigure' + index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)} />);
-      controllerUnits.push(<ControllerUnit key={index} />)
-      // controllerUnits.push(<ControllerUnit key={index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>)
+    
+      controllerUnits.push(<ControllerUnit key={index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>)
     }.bind(this));
 
     return (
